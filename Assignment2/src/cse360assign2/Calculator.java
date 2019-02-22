1bc37148f97d20e94146bdfea7c1baa4ba5d83eb
daniel <daniel@172.20.10.3>
2019-02-22 16:48:12 -0700

    Test for assignment 2 GitHub

---
+++ Assignment2/src/cse360assign2/Calculator.java
@@ -0,0 +1,46 @@
+/*Name: Daniel Martinez
+ *Class ID: 475
+ *Assignment #: 2
+ *Description: 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
